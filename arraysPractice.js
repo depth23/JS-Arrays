c6173617801c6a479ffd53e0f29849ee8c644a1a
Deon Sharps <deonsharps@Deons-MacBook-Air.local>
2015-09-02 08:54:20 -0600

    modified arraysPractice

--- arraysPractice.js
+++ arraysPractice.js
@@ -5,7 +5,9 @@ var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
   //Code Here
-
+function first(arr) {
+  return arr[0];
+}
 
 //Next problem
 
@@ -16,8 +18,10 @@ var arr = [40,50,60];
 
 
   //Code Here
-
-
+function last(arr) {
+   return arr[arr.length - 1];
+}
+last(arr);
 //Next Problem
 
 
@@ -35,7 +39,24 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
+function reversedLooper(letters) {
+    letters.reverse();
+    for (var i =0; i < letters.length; i++) {
+        alert(letters[i]);
+    };
+    
+}
+reversedLooper(letters);
 
+OR
+
+function reversedLooper(letters) {
+    for (var i =4; i > 0; i--) {
+        alert(letters[i]);
+    };
+                                                    <<<===This one is better!
+}
+reversedLooper(letters);
 
 //Next Problem
 
@@ -44,8 +65,20 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
   //Code Here
-
-
+function evenFinder(nums) {
+    // create a new empty array
+    var emptyArray = [];
+    for(i = 0, i < nums.length; i++); {
+        if((nums[i] % 2) != 0) {
+              
+        }
+    };
+    // for loop that checks for evens
+        // push each number to the new array
+    
+    // replace value of nums for new array
+}
+evenFinder(nums);
 //Next problem
 
 
